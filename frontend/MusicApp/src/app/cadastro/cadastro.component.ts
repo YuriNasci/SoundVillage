3269add729b91d60a7166dfdf651f1764042f9a7
Yuri A. Nascimento <yurinascimento@outlook.com>
2024-04-04 00:14:12 -0300

    feat: Cadastro
    Inclusão do componente "Cadastro"

--- frontend/MusicApp/src/app/cadastro/cadastro.component.ts
+++ frontend/MusicApp/src/app/cadastro/cadastro.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatOptionModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatOptionModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
-cadastroForm: any;
+  cadastroForm = new FormGroup({
+    nome: new FormControl('', Validators.required),
+    senha: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    nascimento: new FormControl('', Validators.required),
+    plano: new FormControl('', Validators.required)
+  });
 
+  onSubmit() {
+    if (this.cadastroForm.invalid) {
+      return;
+    }
+  }
 }
