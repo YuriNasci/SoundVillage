98795fa750f2174891b9245a8360b208f67e5ba7
Yuri A. Nascimento <yurinascimento@outlook.com>
2024-05-06 23:13:06 -0300

    feat: Inclusão de Template do Spotify

--- frontend/MusicApp/src/app/app.component.ts
+++ frontend/MusicApp/src/app/app.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatGridListModule} from '@angular/material/grid-list';
-import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule, MatGridListModule, MatInputModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Infnet';
+  title = 'spotify-clone';
 }
